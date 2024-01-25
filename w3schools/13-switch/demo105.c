eae9f9c934e8424949db8ce56c3a39f7da550cee
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-01-24 19:47:07 -0500

    switch w3schools

---
+++ w3schools/13-switch/demo105.c
@@ -0,0 +1,18 @@
+// W3Schools, C Switch
+#include <stdio.h>
+
+int main() {
+    int day = 4;
+
+    switch (day) {
+        case 6:
+            printf("Today is Saturday");
+            break;
+        case 7:
+            printf("Today is Sunday");
+            break;
+        default:
+            printf("Looking forward to the Weekend");
+    }
+    return 0;
+}
